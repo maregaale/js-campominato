8e3eb6ee60b8b394f9aaa7d00924193dbd13ffa5
Alessandro Marega <ale_marega@hotmail.it>
2021-03-09 17:34:00 +0100

    aggiunte difficoltà e fix sintassi con variabile max in base alla difficoltà

--- js/script.js
+++ js/script.js
@@ -1,59 +1,91 @@
-// CONSEGNA:
-
-// BONUS: (da fare solo se funziona tutto il resto)
-// all’inizio il software richiede anche una difficoltà all’utente che cambia il range di numeri casuali:
-// con difficoltà 0 => tra 1 e 100
-// con difficoltà 1 => tra 1 e 80
-// con difficoltà 2 => tra 1 e 50
-
-
-// SVOLGIMENTO:
-
-// genero 16 numeri casuali tra 1 e 100
+// FUNZIONI:
 // funzione per generare numero random da x a y
 function randomGenerator(x, y) {
   return Math.floor(Math.random() * (y - x + 1) ) + x;
 }
 
-// ciclo per 16 volte generando i 16 numeri
+// chiedo di scegliere la difficoltà
+var difficulty = prompt("inserisci la difficoltà: 0(facile), 1(media), 2(difficile)");
+
+// ciclo per 16 volte generando i 16 numeri casuali tra 1 e 100
 var random = [];
-for(var i = 0; i < 16 ; i++) {
 
-  var numberCpu = parseInt(randomGenerator(1, 100));
-  // controllo che il numero non sia già presente
-  if (random.includes(numberCpu) == false) {
-      random.push(numberCpu);
-  } else {
-    i--;
+// inserisco le diverse difficoltà
+if (!(isNaN(difficulty)) && difficulty != "" && 0 <= difficulty && difficulty <= 2)  {
+  // difficoltà: 0
+  if (difficulty == 0) {
+    var max = 100;
+
+    for(var i = 0; i < 16 ; i++) {
+
+      var numberCpu = parseInt(randomGenerator(1, max));
+      // controllo che il numero non sia già presente
+      if (random.includes(numberCpu) == false) {
+          random.push(numberCpu);
+      } else {
+        i--;
+      }
+    }
   }
-}
-console.log(random);
-// chiedo all'utente di inserire un numero alla volta
-var insertUser = [];
+  // difficoltà: 1
+  else if (difficulty == 1) {
+    var max = 80;
 
-// chiedo un numero fintanto che questo non è nella lista dei nueri vietati
+    for(var i = 0; i < 16 ; i++) {
+
+      var numberCpu = parseInt(randomGenerator(1, max));
+      // controllo che il numero non sia già presente
+      if (random.includes(numberCpu) == false) {
+          random.push(numberCpu);
+      } else {
+        i--;
+      }
+    }
+  }
+  // difficoltà: 2
+  else if (difficulty == 2) {
+    var max = 50;
+
+    for(var i = 0; i < 16 ; i++) {
+
+      var numberCpu = parseInt(randomGenerator(1, max));
+      // controllo che il numero non sia già presente
+      if (random.includes(numberCpu) == false) {
+          random.push(numberCpu);
+      } else {
+        i--;
+      }
+    }
+  }
+} else {
+  alert("inserisci una difficoltà in numero tra 0 e 2");
+}
+
+// chiedo un numero alla volta fintanto che questo non è nella lista dei nueri vietati
+var insertUser = [];
 var i = 0;
-while (i <= (100 - random.length) && !(random.includes(numberUser))) {
-  var numberUser = parseInt(prompt("inserisci un numero da 1 a 100"));
+
+while (i <= (max - random.length) && !(random.includes(numberUser))) {
+  var numberUser = parseInt(prompt("inserisci un numero da 1 a " + max));
 
   // controllo che l'utente non inserisca lo stesso numero
   if (!(isNaN(numberUser)) && numberUser != "") {
-    if (numberUser >= 1 && numberUser <= 100 ) {
+    if (numberUser >= 1 && numberUser <= max ) {
       if (insertUser.includes(numberUser) == false) {
         insertUser.push(numberUser);
       } else {
         alert("non ripetere lo stesso numero");
       }
     } else {
-      alert("devi inserire un numero da 1 a 100");
+      alert("devi inserire un numero da 1 a " + max);
     }
   } else {
     alert("devi inserire un numero");
   }
-
 
   i++;
 }
 
 // stampo il punteggio raggiunto dall'utente
 console.log("il tuo punteggio è di " + (insertUser.length - 1));
+console.log("le bombe erano queste: " + random);
